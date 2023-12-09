5747b6d4ed9b7260f0bd289809dd113250e31f33
dmytro.didukh <dmytro.didukh9@gmail.com>
2023-12-09 13:47:28 +0200

    Add password visibility toggle and update .gitignore
    Extended the FormField component functionality in form-field.tsx to include a password visibility toggle. Introduced a state variable to control visibility of password inputs. Also added .idea directory to .gitignore file to exclude JetBrains IDE settings from version control. These changes improve user experience and project cleanliness respectively.

--- src/components/form-field/form-field.tsx
+++ src/components/form-field/form-field.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import {
+    Button,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputRightElement,
+} from '@chakra-ui/react';
 import { Field, useField } from 'formik';
 import React from 'react';
 
@@ -11,14 +19,31 @@ interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 // TODO: Fix console error
 // app-index.js:32 Warning: Extra attributes from the server: data-new-gr-c-s-check-loaded,data-gr-ext-installed
 const FormField = ({ id, name, label, placeholder, type = 'text' }: FormFieldProps) => {
+    const [show, setShow] = React.useState(false);
     const [field, { error, touched }] = useField(name);
+
+    const handleClick = () => setShow(!show);
 
     return (
         <Field name={name}>
             {() => (
                 <FormControl isInvalid={!!error && touched}>
                     <FormLabel htmlFor={id}>{label}</FormLabel>
-                    <Input {...field} id={id} placeholder={placeholder} type={type} />
+                    <InputGroup size="md">
+                        <Input
+                            {...field}
+                            id={id}
+                            placeholder={placeholder}
+                            type={show ? 'text' : type}
+                        />
+                        {type === 'password' && (
+                            <InputRightElement width="4.5rem">
+                                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                                    {show ? 'Hide' : 'Show'}
+                                </Button>
+                            </InputRightElement>
+                        )}
+                    </InputGroup>
                     <FormErrorMessage>{error}</FormErrorMessage>
                 </FormControl>
             )}
