23cedfd56dc95679a08f8af4dc26be6cfbe7a628
dmytro.didukh <dmytro.didukh9@gmail.com>
2024-01-13 14:38:13 +0200

    refactor

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -18,9 +18,7 @@ class DateUtils {
     }
 
     static formatToRelativeTime(date: PossibleDate) {
-        const now = new Date();
-        const _date = this.toDateInstance(date);
-        const diffInSeconds = Math.floor((now.getTime() - _date.getTime()) / 1000);
+        const diffInSeconds = this.getTimeDifferenceInSecondsFromNow(date);
 
         let value: number;
         let unit: Intl.RelativeTimeFormatUnit;
@@ -39,7 +37,7 @@ class DateUtils {
             unit = 'hour';
         } else {
             // If more than 24 hours, return the full date.
-            return this.formatToLongDate(_date);
+            return this.formatToShortDate(date);
         }
 
         const rtf = new Intl.RelativeTimeFormat('en-US', { numeric: 'auto' });
@@ -53,8 +51,14 @@ class DateUtils {
         return this.toDateInstance(date).toLocaleDateString('en-US', options);
     }
 
-    static toDateInstance(date: PossibleDate): Date {
-        return new Date(date);
+    private static getTimeDifferenceInSecondsFromNow(date: PossibleDate): number {
+        const now = new Date();
+        const _date = this.toDateInstance(date);
+        return Math.floor((now.getTime() - _date.getTime()) / 1000);
+    }
+
+    private static toDateInstance(date: PossibleDate): Date {
+        return date instanceof Date ? date : new Date(date);
     }
 }
 
